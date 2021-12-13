2b7868be1454a276f3665665c8e1fb549a0d73a6
Sunandan <sunandanmahant@outlook.com>
2021-12-12 21:09:05 -0500

    Fixed Group List and Added Temperature Loop

--- src/AIOMFAC.jl
+++ src/AIOMFAC.jl
@@ -28,58 +28,58 @@ const group = OrderedDict([
 	("CH2", 2),
 	("CH", 3),
 	("C", 4),
-	("CH2CH", 5),
-	("CHCH", 6),
-	("CH2C", 7),
-	("CHC", 8),
-	("CC", 70),
+	("CH2=CH", 5),
+	("CH=CH", 6),
+	("CH2=C", 7),
+	("CH=C", 8),
+	("C=C", 70),
 	("ACH", 9),
 	("AC", 10),
 	("OH", 153),
 	("ACOH", 17),
 	("CH3CO", 18),
 	("CH2CO", 19),
-	("CHO[ALDEHYDE]", 20),
+	("CHO[aldehyde]", 20),
 	("CH3COO", 21),
 	("CH2COO", 22),
 	("CH3O", 24),
 	("CH2O", 25),
-	("CHO[ETHER]", 26),
+	("CHO[ether]", 26),
 	("THF[CH20]", 27),
 	("COOH", 137),
 	("HCOOH", 43),
-	("CH3[ALC]", 141),
-	("CH2[ALC]", 142),
-	("CH[ALC]", 143),
-	("C[ALC]", 144),
-	("CH3[ALC-TAIL]", 145),
-	("CH2[ALC-TAIL]", 146),
-	("CH[ALC-TAIL]", 147),
-	("C[ALC-TAIL", 148),
-	("CH3OH", 149),
-	("CH2OH", 150),
-	("CHOH", 151),
-	("COH", 152),
-	("CHOCH2[OXYETHYLENE]", 154),
+	("CH3[alc]", 141),
+	("CH2[alc]", 142),
+	("CH[alc]", 143),
+	("C[alc]", 144),
+	("CH3[alc-tail]", 145),
+	("CH2[alc-tail]", 146),
+	("CH[alc-tail]", 147),
+	("C[alc-tail]", 148),
+	("CH3[OH]", 149),
+	("CH2[OH]", 150),
+	("CH[OH]", 151),
+	("C[OH]", 152),
+	("CH2OCH2[oxyethylene]", 154),
 	("CH2ONO2", 155),
 	("CHONO2", 156),
 	("CONO2", 157),
-	("CH2OOH[PEROX]", 158),
-	("CHOOH[PEROX]", 159),
-	("COOH[PEROX]", 160),
-	("C(=O)OOH[PEROX]", 161),
-	("CH3OOCH3[PEROX]", 162),
-	("CH3OOCH2[PEROX]", 163),
-	("CH3OOCH[PEROX]", 164),
-	("CH3OOC[PEROX]", 165),
-	("CH2OOCH2[PEROX]", 166),
-	("CH2OOCH[PEROX]", 167),
-	("CH2OOC[PEROX]", 168),
-	("CHOOCH[PEROX]", 169),
-	("CHOOC[PEROX]", 170),
-	("COOC[PPEROX]", 171),
-	("C(=O)OONO2[PEROX]", 172),
-	("CO2[AQ]", 173),
+	("CH2OOH[perox]", 158),
+	("CHOOH[perox]", 159),
+	("COOH[perox]", 160),
+	("C(=O)OOH[perox]", 161),
+	("CH3OOCH3[perox]", 162),
+	("CH3OOCH2[perox]", 163),
+	("CH3OOCH[perox]", 164),
+	("CH3OOC[perox]", 165),
+	("CH2OOCH2[perox]", 166),
+	("CH2OOCH[perox]", 167),
+	("CH2OOC[perox]", 168),
+	("CHOOCH[perox]", 169),
+	("CHOOC[perox]", 170),
+	("COOC[perox]", 171),
+	("C(=O)OONO2[perox]", 172),
+	("CO2[aq]", 173),
 	("H+", 205),
 	("Li+", 201),
 	("Na+", 202), 
@@ -104,14 +104,15 @@ const group = OrderedDict([
 	("Glut--", 264)
 ])
 
+
 function writeinput(file, Tk, components, fractions)
-	fi = 0.001:0.01:0.9 
+	fi = 1.0:-0.2:0.2
 	open("Inputfiles/"*file, "w") do f
 		write(f, header)
 		map(1:length(components)) do i
 			compound = components[i]
 			write(f, "component no.:	$(i+1)\n")
-			write(f, "component name:	'inorganic component'\n")
+			write(f, "component name:	'organic component'\n")
 			map(compound) do x
 				no = group[x[1]]
 				write(f, "subgroup no., qty: $(no), $(x[2])\n")
@@ -120,12 +121,14 @@ function writeinput(file, Tk, components, fractions)
 		end
 		write(f, units)
 		write(f, "point, Tk, cp0, ...\n")
-		map(fi) do x
-			write(f, "1	    $(Tk),     ")
-			map(fractions) do y
-				write(f, "$(x*y/sum(fractions)),    ")
-			end
-			write(f,"\n")
+		for z in Tk
+			map(fi) do x
+				write(f, "1	    $z,     ")
+				map(fractions) do y
+					write(f, "$(x*y/sum(fractions)),    ")
+				end
+				write(f,"\n")
+			end	
 		end
 		write(f, "====")
 	end
@@ -142,7 +145,7 @@ end
 
 function parseoutput(file)
 	lines = readlines("Outputfiles/"*file) 
-	out = mapfoldl(hcat, lines[141:228]) do x
+	out = mapfoldl(hcat, lines[67:81]) do x
 		map(mparse, split(x))
 	end
 	aw = out[3,:]./100.0
