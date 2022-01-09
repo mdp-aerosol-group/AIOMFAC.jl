434112ce6948f217ac2efa2565a88b3abe8cf12c
Markus Petters <mdpetter@ncsu.edu>
2022-01-09 11:00:43 +0100

    refactor tests to print output and reduce code len gth

--- Task2/Tests.jl
+++ Task2/Tests.jl
@@ -9,72 +9,54 @@ components = [(("COOH", 3), ("CH2", 2), ("C", 1), ("OH", 1))]
 
 # To generate output
 function program(file)
-    AIOMFAC.writeinput(file, Tk, fi, components, 1.0)
-    run(`./AIOMFAC Inputfiles/$file`)
-    fil = file[7:10]
-    fileo = "AIOMFAC_output_$fil.txt"
-    return fileo
+	AIOMFAC.writeinput(file, Tk, fi, components, 1.0)
+	run(`./AIOMFAC Inputfiles/$file`)
+	fil = file[7:10]
+	fileo = "AIOMFAC_output_$fil.txt"
+	return fileo
 end
 
 # To compare two files
 function test_output(f1, f2)
-    a = (""*f1)
-    b = (""*f2)
-    u = Output.Viscosity(a, Tk, fi)
-    v = Output.Viscosity(b, Tk, fi)
-    x = Output.Components(a, Tk, fi)
-    y = Output.Components(b, Tk, fi)
-    @test u.T==v.T  
-    @test u.RH == v.RH
-    @test u.η == v.η    
-    @test u.ση == v.ση
-    @test u.flag == v.flag
+	println("")
+	split(f1, "/") |> x -> println("Testing: "*x[end])
+	u = Output.Viscosity(f1, Tk, fi)
+	v = Output.Viscosity(f2, Tk, fi)
 
-    ind = length(x)
+	@testset "Testing Phase Ouptut" begin
+		@test u.T==v.T  
+		@test u.RH == v.RH
+		@test u.η == v.η    
+		@test u.ση == v.ση
+		@test u.flag == v.flag
+	end
 
-    count = 1
-    while count <= ind
-        @test x[count].T == y[count].T
-        @test x[count].RH == y[count].RH
-        @test x[count].w == y[count].w
-        @test x[count].xᵢ == y[count].xᵢ
-        @test x[count].mᵢ == y[count].mᵢ
-        @test x[count].γ == y[count].γ
-        @test x[count].a == y[count].a
-        @test x[count].flag == y[count].flag
-        count += 1    
-    end        
+	function component_test(x,y)
+		@test x.T == y.T
+		@test x.RH == y.RH
+		@test x.w == y.w
+		@test x.xᵢ == y.xᵢ
+		@test x.mᵢ == y.mᵢ
+		@test x.γ == y.γ
+		@test x.a == y.a
+		@test x.flag == y.flag
+	end
+
+	x = Output.Components(f1, Tk, fi)
+	y = Output.Components(f2, Tk, fi)
+	
+	@testset "Testing Component Output" begin
+		map(component_test, x, y)
+	end
 end     
 
 # To compare all files generated from the web
 comp = ["Citric_Acid.txt", "Formic_Acid.txt", "Acetone.txt", "Ethanol.txt", "Phenol.txt"]
-
-function test_all(comp)
-    path1 = "../Web Output/Predefined List/"
-    path2 = "../Web Output/Defined Subgroups/"
-
-    ind = length(comp)
-    count = 1
-    while count <= ind
-        a = path1*comp[count]
-        b = path2*comp[count]
-        test_output(a,b)
-        count += 1
-    end    
-end
-
-# Comparing all files from web
-test_all(comp)
-
-# Comparing two files individually
-test_output("../Web Output/Predefined List/Citric_Acid.txt", "../Web Output/Defined Subgroups/Citric_Acid.txt")
+path1 = "../Web Output/Predefined List/"
+path2 = "../Web Output/Defined Subgroups/"
+map(test_output, path1.*comp, path2.*comp)
 
 # Comparing web output and programmed output
+println("")
+println("Testing Local Install")
 test_output("../Web Output/Predefined List/Citric_Acid.txt", program("input_0008.txt"))
-
-# Phase and component data
-phase, compo = Output.load_data("AIOMFAC_output_0010.txt", Tk, fi)
-
-# Dataframe for phase and component
-dfm0 = Output.dfvis("AIOMFAC_output_0010.txt", Tk, fi)
-dfm1 = Output.dfcom("AIOMFAC_output_0010.txt", Tk, fi, 2)
